ce3c1c3594cf356327b631873fdad0edca50a8e5
Antonio Murcia <amurciaa61@gmail.com>
2020-10-09 02:52:40 +0200

    Agregar archivos de proyecto.

---
+++ 4_CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _4_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int MAXIMA_LONGITUD_TEXTO = 140;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contadorTextBlock.Text = "0/" + MAXIMA_LONGITUD_TEXTO;
+        }
+
+        private void textoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int longitud = textoTextBox.Text.Length;
+            contadorTextBlock.Text = longitud + "/" + MAXIMA_LONGITUD_TEXTO;
+            if (longitud >= MAXIMA_LONGITUD_TEXTO)
+                textoTextBox.IsReadOnly = true;
+        }
+    }
+}
